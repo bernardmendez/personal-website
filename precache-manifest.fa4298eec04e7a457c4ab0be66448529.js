16bb7aac64be5676359e84de453f193f87da665d
bernardmendez <bernard.h.mendez@gmail.com>
2020-09-29 19:30:56 -0700

    Updates

---
+++ precache-manifest.fa4298eec04e7a457c4ab0be66448529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53923ba43be6558cc7bd8a97a6047fc",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "f13f97231bf399bbfe6c",
+    "url": "/personal-website/static/css/main.76f2c86a.chunk.css"
+  },
+  {
+    "revision": "97061f55fcd24c242bc0",
+    "url": "/personal-website/static/js/2.53d86f77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.53d86f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13f97231bf399bbfe6c",
+    "url": "/personal-website/static/js/main.03373b3f.chunk.js"
+  },
+  {
+    "revision": "6df706e2825f0c60d52b",
+    "url": "/personal-website/static/js/runtime-main.2ce961a5.js"
+  }
+]);
